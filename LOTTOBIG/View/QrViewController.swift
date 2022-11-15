88c2157b92b7b384039625f654daa3f1514dfebf
Chord-West <gustj2005@naver.com>
2022-11-15 21:36:10 +0900

    Initial Commit

---
+++ LOTTOBIG/View/QrViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QrViewController.swift
+//  LOTTOBIG
+//
+//  Created by 김현서 on 2022/11/14.
+//
+
+import UIKit
+
+
+class QrViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ LOTTOBIG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOTTOBIG
+//
+//  Created by 김현서 on 2022/11/14.
+//
+
+import UIKit
+
+class VsiewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
