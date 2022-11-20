74bfd26814e79fd5c953759fa96031bc2149c160
Chord-West <gustj2005@naver.com>
2022-11-20 22:57:57 +0900

    add ReaderView into QrviewController

--- LOTTOBIG/View/ReaderView.swift
+++ LOTTOBIG/View/ReaderView.swift
@@ -2,118 +2,162 @@
 //  ReaderView.swift
 //  LOTTOBIG
 //
-//  Created by 김현서 on 2022/11/17.
+//  Created by 김현서 on 2022/11/20.
 //
 
 import UIKit
 import AVFoundation
 
-class QRCodeReaderViewController : UIViewController{
-    // 1. 실시간 캡처를 수행하기 위해서 AVCaptureSession 개체로 인스턴스화
-    
-    private let captureSession = AVCaptureSession()
-    
-    override func viewDidLoad(){
-        super.viewDidLoad()
-        
-        basicSetting()
-    }
-    
+enum ReaderStatus {
+    case success(_ code: String?)
+    case fail
+    case stop(_ isButtonTap: Bool)
 }
 
+protocol ReaderViewDelegate: class {
+    func readerComplete(status: ReaderStatus)
+}
 
-extension QRCodeReaderViewController {
-    private func basicSetting(){
-        // AVCaptureDevice : caputre sessions 에 대한 입력 ( audio or video )과 하드웨어별 캡쳐 기능에 대한 제어를 제공하는 장치
-        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else{ // 사물을 video 방식으로 영상 촬영
-            // 시뮬레이터에서는 카메라를 사용할 수 없기 때문에 시뮬레이터에서 실행하면 에러가 발생한다.
-            fatalError("No Video device found")
+class ReaderView: UIView {
+
+    weak var delegate: ReaderViewDelegate?
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var centerGuideLineView: UIView?
+    
+    var captureSession: AVCaptureSession?
+    
+    var isRunning: Bool {
+        guard let captureSession = self.captureSession else {
+            return false
         }
+
+        return captureSession.isRunning
+    }
+
+    let metadataObjectTypes: [AVMetadataObject.ObjectType] = [.upce, .code39, .code39Mod43, .code93, .code128, .ean8, .ean13, .aztec, .pdf417, .itf14, .dataMatrix, .interleaved2of5, .qr]
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.initialSetupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.initialSetupView()
+    }
+    
+    private func initialSetupView() {
+        self.clipsToBounds = true
+        self.captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        
         do {
-
-            let input = try AVCaptureDeviceInput(device: captureDevice) //
-            captureSession.addInput(input)
-
-            // 3️⃣ 적절한 outputs 설정
-            // ✅ AVCaptureMetadataOutput : capture session 에 의해서 생성된 시간제한 metadata 를 처리하기 위한 capture output.
-            // ✅ 즉, 영상으로 촬영하면서 지속적으로 생성되는 metadata 를 처리하는 output 이라는 말.
-            let output = AVCaptureMetadataOutput()
-
-            // ✅ session 에 주어진 output 를 추가.
-            captureSession.addOutput(output)
-
-            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-
-            output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-
-            // 카메라 영상이 나오는 layer 와 + 모양 가이드 라인을 뷰에 추가하는 함수 호출.
-            setVideoLayer()
-            setGuideCrossLineView()
-
-            // 4️⃣ startRunning() 과 stopRunning() 로 흐름 통제
-            // ✅ input 에서 output 으로의 데이터 흐름을 시작
-            captureSession.startRunning()
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch let error {
+            print(error.localizedDescription)
+            return
         }
-        catch {
-            print("error")
+
+        guard let captureSession = self.captureSession else {
+            self.fail()
+            return
         }
-    }
-    private func setVideoLayer() {
-        // 영상을 담을 공간.
-        let videoLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        // 카메라의 크기 지정
-        videoLayer.frame = view.layer.bounds
-        // 카메라의 비율지정
-        videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        view.layer.addSublayer(videoLayer)
-        captureSession.startRunning()
+
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            self.fail()
+            return
+        }
         
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = self.metadataObjectTypes
+        } else {
+            self.fail()
+            return
+        }
+        
+        self.setPreviewLayer()
+        self.setCenterGuideLineView()
     }
 
-    // ✅ + 모양 가이드라인을 뷰에 추가
-    private func setGuideCrossLineView() {
-        let guideCrossLine = UIImageView()
-        guideCrossLine.image = UIImage(systemName: "plus")
-        guideCrossLine.tintColor = .green
-        guideCrossLine.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(guideCrossLine)
-        NSLayoutConstraint.activate([
-            guideCrossLine.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            guideCrossLine.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            guideCrossLine.widthAnchor.constraint(equalToConstant: 30),
-            guideCrossLine.heightAnchor.constraint(equalToConstant: 30)
-        ])
+    private func setPreviewLayer() {
+        guard let captureSession = self.captureSession else {
+            return
+        }
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.frame = self.layer.bounds
+
+        self.layer.addSublayer(previewLayer)
+
+        self.previewLayer = previewLayer
+    }
+
+    private func setCenterGuideLineView() {
+        let centerGuideLineView = UIView()
+        centerGuideLineView.translatesAutoresizingMaskIntoConstraints = false
+        centerGuideLineView.backgroundColor = #colorLiteral(red: 1, green: 0.5411764706, blue: 0.2392156863, alpha: 1)
+        self.addSubview(centerGuideLineView)
+        self.bringSubviewToFront(centerGuideLineView)
+
+        centerGuideLineView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        centerGuideLineView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        centerGuideLineView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        centerGuideLineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+
+        self.centerGuideLineView = centerGuideLineView
     }
 }
 
+extension ReaderView {
+    func start() {
+        self.captureSession?.startRunning()
+    }
+    
+    func stop(isButtonTap: Bool) {
+        self.captureSession?.stopRunning()
+        
+        self.delegate?.readerComplete(status: .stop(isButtonTap))
+    }
+    
+    func fail() {
+        self.delegate?.readerComplete(status: .fail)
+        self.captureSession = nil
+    }
+    
+    func found(code: String) {
+        self.delegate?.readerComplete(status: .success(code))
+    }
+}
 
-extension QRCodeReaderViewController: AVCaptureMetadataOutputObjectsDelegate {
-
-    // ✅ caputure output object 가 새로운 metadata objects 를 보냈을 때 알린다.
-    func metadataOutput(_ captureOutput: AVCaptureMetadataOutput,
-                        didOutput metadataObjects: [AVMetadataObject],
-                        from connection: AVCaptureConnection) {
-
-        // ✅ metadataObjects : 새로 내보낸 AVMetadataObject 인스턴스 배열이다.
+extension ReaderView: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        stop(isButtonTap: false)
+        
         if let metadataObject = metadataObjects.first {
-
-            // ✅ AVMetadataObject 는 추상 클래스이므로 이 배열의 object 는 항상 구체적인 하위 클래스의 인스턴스여야 한다.
-            // ✅ AVMetadataObject 를 직접 서브클래싱해선 안된다. 대신 AVFroundation 프레임워크에서 제공하는 정의된 하위 클래스 중 하나를 사용해야 한다.
-            // ✅ AVMetadataMachineReadableCodeObject : 바코드의 기능을 정의하는 AVMetadataObject 의 구체적인 하위 클래스. 인스턴스는 이미지에서 감지된 판독 가능한 바코드의 기능과 payload 를 설명하는 immutable object 를 나타낸다.
-            // ✅ (참고로 이외에도 AVMetadataFaceObject 라는 감지된 단일 얼굴의 기능을 정의하는 subclass 도 있다.)
-            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject, let stringValue = readableObject.stringValue else {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject,
+                let stringValue = readableObject.stringValue else {
                 return
             }
 
-            // ✅ qr코드가 가진 문자열이 URL 형태를 띈다면 출력.(아무런 qr코드나 찍는다고 출력시키면 안되니까 여기서 분기처리 가능. )
-            if stringValue.hasPrefix("http://") || stringValue.hasPrefix("https://")  {
-                print(stringValue)
-
-                // 4️⃣ startRunning() 과 stopRunning() 로 흐름 통제
-                // ✅ input 에서 output 으로의 흐름 중지
-                self.captureSession.stopRunning()
-                self.dismiss(animated: true, completion: nil)
-            }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
         }
     }
 }
