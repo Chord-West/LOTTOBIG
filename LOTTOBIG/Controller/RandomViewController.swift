33ec7c52fd363f74f4b95657d564bc36867fd669
Chord-West <gustj2005@naver.com>
2022-11-29 23:02:47 +0900

    add function random generate number

--- LOTTOBIG/Controller/RandomViewController.swift
+++ LOTTOBIG/Controller/RandomViewController.swift
@@ -18,23 +18,60 @@ class RandomViewController : UIViewController {
     @IBOutlet weak var sixthBallLabel: UILabel!
     
     var numbers:[Int] = []
+    var BallLabels:[UILabel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 6개의 공이 동일한 UI
-
-        [firstBallLabel, secondBallLabel, thirdBallLabel,
-         fourthBallLabel, fifthBallLabel, sixthBallLabel].forEach{
-            $0?.layer.borderWidth = 1
-            $0?.layer.cornerRadius = CGRect($0?.frame)/2
+        BallLabels  = [firstBallLabel, secondBallLabel, thirdBallLabel,
+         fourthBallLabel, fifthBallLabel, sixthBallLabel]
+        BallLabels.forEach{
+            $0.layer.borderWidth = 1
+            $0.layer.cornerRadius = 30
         }
 
+
+    }
+    
+    
+    func getRandomNumber() -> Int{
+        let random_num: Int = Int(arc4random_uniform(45)) + 1
+        return random_num
+    }
+    
+    func generateNumbers(){
+        while numbers.count < 6{
+            let num:Int = getRandomNumber()
+            
+            if numbers.contains(num){
+                continue
+            }else{
+                numbers.append(num)
+            }
+        }
+    }
+    
+    func connectLabelColor(num: Int) -> UIColor {
+        var colors: [UIColor]
+
+        colors = UIColor.colorWithString("F9264E")
     }
     
     
     @IBAction func randomButtonTapped(_ sender: UIButton) {
         print("hi")
+        numbers = []
+        generateNumbers()
+        numbers.sort()
+
+        for i in 0...5 {
+            BallLabels[i].text = String(numbers[i])
+            
+            
+            BallLabels[i].layer.backgroundColor = connectLabelColor(num:numbers[i])
+        }
+        
         
     }
     
