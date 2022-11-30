b81cb375e4a1606f09bc0ccc6ddab1d7897b3225
Chord-West <gustj2005@naver.com>
2022-11-30 21:55:47 +0900

    add Connected UILabel and UIColor to  Use Switch syntax

--- LOTTOBIG/Controller/RandomViewController.swift
+++ LOTTOBIG/Controller/RandomViewController.swift
@@ -27,8 +27,9 @@ class RandomViewController : UIViewController {
         BallLabels  = [firstBallLabel, secondBallLabel, thirdBallLabel,
          fourthBallLabel, fifthBallLabel, sixthBallLabel]
         BallLabels.forEach{
-            $0.layer.borderWidth = 1
-            $0.layer.cornerRadius = 30
+            $0.layer.masksToBounds = true
+            $0.layer.cornerRadius = $0.frame.size.height/2.0
+            
         }
 
 
@@ -53,9 +54,23 @@ class RandomViewController : UIViewController {
     }
     
     func connectLabelColor(num: Int) -> UIColor {
-        var colors: [UIColor]
-
-        colors = UIColor.colorWithString("F9264E")
+        
+        switch num {
+            
+        case 1...10:
+            return UIColor(red: 255/255, green: 200/255, blue: 0/255, alpha: 1.0)
+        case 11...20:
+            return UIColor(red: 100/255, green: 200/255, blue: 245/255, alpha: 1.0)
+        case 21...30:
+            return UIColor(red: 255/255, green: 110/255, blue: 110/255, alpha: 1.0)
+        case 31...40:
+            return UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1.0)
+        case ...45:
+            return UIColor(red: 167/255, green: 230/255, blue: 56/255, alpha: 1.0)
+        default:
+            return UIColor.yellow
+        }
+    
     }
     
     
@@ -67,9 +82,11 @@ class RandomViewController : UIViewController {
 
         for i in 0...5 {
             BallLabels[i].text = String(numbers[i])
+            BallLabels[i].textColor = .white
+            BallLabels[i].font = UIFont.systemFont(ofSize: 20, weight: .bold)
+            BallLabels[i].layer.backgroundColor = connectLabelColor(num:numbers[i]).cgColor
             
             
-            BallLabels[i].layer.backgroundColor = connectLabelColor(num:numbers[i])
         }
         
         
