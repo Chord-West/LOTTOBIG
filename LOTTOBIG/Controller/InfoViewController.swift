ec9b645b7b9c09edac1eb301b912c76ac27cff42
Chord-West <gustj2005@naver.com>
2023-01-02 22:50:43 +0900

    add : 동행복권 api 연결할 Model 추가 및 데이터 통신

--- LOTTOBIG/Controller/InfoViewController.swift
+++ LOTTOBIG/Controller/InfoViewController.swift
@@ -14,6 +14,7 @@ class InfoViewController : UIViewController{
     
     @IBOutlet weak var lookUpBtn: UIButton!
     
+    
     override func viewDidLoad(){
         super.viewDidLoad()
     }
@@ -24,11 +25,16 @@ class InfoViewController : UIViewController{
     
     func alamofire() {
         // HTTP 네트워킹을 통해 전송 할 데이터
-        AF.request("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+1", method: .get, encoding: URLEncoding.default, headers: nil).validate().responseData { response in
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+1"
+        AF.request(url).validate().responseJSON { (response) in
                 switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    print(json)
+                case .success(let obj):
+                    if let nsDictionary = obj as? NSDictionary {
+                        if let drwtNo1 = nsDictionary["drwtNo1"] as? Int {
+                            print(drwtNo1)
+                        }
+                    }
+                    //print(json.drwtNo1)
                 case .failure(let error):
                     print("\(#function): \(error)")
                 }
---
+++ LOTTOBIG/Model/LottoBall.swift
@@ -0,0 +1,27 @@
+//
+//  LottoBall.swift
+//  LOTTOBIG
+//
+//  Created by 김현서 on 2023/01/02.
+//
+
+import Foundation
+
+
+struct LottoBall : Codable{
+    var drwtNo1 : Int?
+    var drwtNo2 : Int?
+    var drwtNo3 : Int?
+    var drwtNo4 : Int?
+    var drwtNo5 : Int?
+    var drwtNo6 : Int?
+    var bnusNo : Int?
+    var drwNoDate : String?
+    var firstAccumamnt : Int?
+    var firstPrzwnerCo : Int?
+    var firstWinamnt  : Int?
+    var returnValue : Int?
+    var totSellamnt : Int?
+    
+    
+}
